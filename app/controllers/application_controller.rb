a313aabfae92d0a746af9c769a1b6c24e9e531ed
Rennan Maciel <renmcoliver@gmail.com>
2015-12-14 14:28:27 -0200

    Adicionando comentário em application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :sso_filter
+  # A gem do portal te dá os seguintes métodos:
+
+  # usuario_corrente (com usuario_corrente.iduff e usuario_corrente.ip)
+  # usuario_logado? (true ou false, status do login no portal)
 
 
 end
