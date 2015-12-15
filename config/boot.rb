40a276d2522941afbef66a1502e59bb36621e075
Bruno Neves <bnbelizario@gmail.com>
2015-12-15 14:27:35 -0200

    Configurando o arquivo boot.rb para utilizar o encoding UTF-8

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,8 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+# -*- encoding : utf-8 -*-
+require 'rubygems'
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
+# Set up gems listed in the Gemfile.
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+ENV['NLS_LANG'] = 'BRAZILIAN PORTUGUESE_BRAZIL.UTF8'
+
