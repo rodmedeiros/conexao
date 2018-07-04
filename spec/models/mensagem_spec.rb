7b8e7aa53b467ad23285b51c8d8150a9791625d0
rodrigo medeiros <rodrigo_medeiros@id.uff.br>
2018-07-04 18:21:05 -0300

    testes de modelo finalizados

--- spec/models/mensagem_spec.rb
+++ spec/models/mensagem_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Mensagem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:mensagem) {Mensagem.new}
+
+  describe 'validations' do
+    subject {mensagem}
+    it { is_expected.to validate_presence_of(:descricao)}
+  end
 end
