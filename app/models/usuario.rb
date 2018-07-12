9f1e99321b4de19ade50ac16b0d16cd6af35cbd9
Peter Leiva <pleiva@id.uff.br>
2018-07-12 12:10:17 -0300

    Conserta erro que impedia usuarios de autenticar

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,8 +1,11 @@
 class Usuario < ApplicationRecord
+  has_secure_password
+  
   has_many :topicos
   has_many :postagems
   has_many :participacao_conversas
 
   validates :iduff, :tipo, :nome, presence: true
   validates :iduff, :cpf, uniqueness: true
+
 end
