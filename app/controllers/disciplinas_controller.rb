1eafa6296b35a3042ddccd05e27e2829b6603d89
Peter Leiva <pleiva@id.uff.br>
2018-07-18 14:05:18 -0300

    Adiciona estrutura para componente de interface para info de turma

--- app/controllers/disciplinas_controller.rb
+++ app/controllers/disciplinas_controller.rb
@@ -5,19 +5,22 @@ class DisciplinasController < ApplicationController
       nome: 'Introdução a Computação Móvel',
       semestre: '2018.2',
       professor: 'Flávia',
-      situacao: 'inscrito'
+      situacao: 'inscrito',
+      turma: 'AA'
     }
     @disciplinas << {
       nome: 'Banco de dados não convencionais',
       semestre: '2018.2',
       professor: 'Victor',
-      situacao: 'pendente'
+      situacao: 'pendente',
+      turma: 'BB'
     }
     @disciplinas << {
       nome: 'Cálculo II-a',
       semestre: '2018.2',
       professor: 'Augusto',
-      situacao: 'inscrito'
+      situacao: 'inscrito',
+      turma: 'A1'
     }
   end
 end
