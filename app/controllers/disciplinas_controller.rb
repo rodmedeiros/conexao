0193bcfbd864704df582c70ea5bd8d932be60591
Peter Leiva <pleiva@id.uff.br>
2018-07-18 13:02:26 -0300

    Adiciona componentes de interface visualizar todas disciplinas

--- app/controllers/disciplinas_controller.rb
+++ app/controllers/disciplinas_controller.rb
@@ -1,4 +1,23 @@
 class DisciplinasController < ApplicationController
   def index
+    @disciplinas = []
+    @disciplinas << {
+      nome: 'Introdução a Computação Móvel',
+      semestre: '2018.2',
+      professor: 'Flávia',
+      situacao: 'inscrito'
+    }
+    @disciplinas << {
+      nome: 'Banco de dados não convencionais',
+      semestre: '2018.2',
+      professor: 'Victor',
+      situacao: 'pendente'
+    }
+    @disciplinas << {
+      nome: 'Cálculo II-a',
+      semestre: '2018.2',
+      professor: 'Augusto',
+      situacao: 'inscrito'
+    }
   end
 end
